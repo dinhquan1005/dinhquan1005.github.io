750ee183eef7bc84984fb633fa4388ceec048f4e
dinhquan1005 <dgcaothu@gmail.com>
2024-04-27 22:33:22 +0700

    init dinhquan1005

---
+++ script.js
@@ -0,0 +1,27 @@
+// Lấy tham chiếu đến phần tử input
+var dungButton = document.getElementById("dung");
+
+// Thiết lập thuộc tính onclick để mở liên kết khi nút được nhấp vào
+dungButton.onclick = function () {
+  window.open(
+    "https://www.facebook.com/profile.php?id=100057810002228", //Rai nè
+    "_blank"
+  );
+};
+
+var giaoButton = document.getElementById("giao");
+
+// Thiết lập thuộc tính onclick để mở liên kết khi nút được nhấp vào
+giaoButton.onclick = function () {
+  window.open(
+    "https://www.facebook.com/profile.php?id=100074224008937", //Rin nè
+    "_blank"
+  );
+};
+
+var thaoButton = document.getElementById("thao");
+
+// Thiết lập thuộc tính onclick để mở liên kết khi nút được nhấp vào
+thaoButton.onclick = function () {
+  window.open("https://www.facebook.com/L.T.G.Thao", "_blank"); //Yu nè
+};
